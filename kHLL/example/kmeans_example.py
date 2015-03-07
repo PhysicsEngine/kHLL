63a3e1c338dd9f71f1917ed76f453dad768c6e19
Lewuathe <lewuathe@me.com>
2015-03-07 23:38:11 +0900

    Create ipython notebook

--- kHLL/example/kmeans_example.py
+++ kHLL/example/kmeans_example.py
@@ -4,24 +4,80 @@ from sklearn import datasets
 from sklearn.cluster import KMeans
 
 def main():
+    print(__doc__)
+
+
+    # Code source: Gaël Varoquaux
+    # Modified for documentation by Jaques Grobler
+    # License: BSD 3 clause
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+
+    from sklearn.cluster import KMeans
+    from sklearn import datasets
+
+    np.random.seed(5)
+
+    centers = [[1, 1], [-1, -1], [1, -1]]
     iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
 
-    X = iris.data  # we only take the first two features.
-    Y = iris.target
+    estimators = {'k_means_iris_3': KMeans(n_clusters=3),
+                  'k_means_iris_8': KMeans(n_clusters=8),
+                  'k_means_iris_bad_init': KMeans(n_clusters=3, n_init=1,
+                                                  init='random')}
 
-    model = KMeans(n_clusters=10,
-                   init='k-means++',
-                   n_init=10,
-                   max_iter=300,
-                   tol=0.0001,
-                   precompute_distances=True,
-                   verbose=0,
-                   random_state=None,
-                   copy_x=True,
-                   n_jobs=1)
-    model.fit(X)
 
-    print(model.labels_)
+    fignum = 1
+    for name, est in estimators.items():
+        fig = plt.figure(fignum, figsize=(4, 3))
+        plt.clf()
+        ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+        plt.cla()
+        est.fit(X)
+        labels = est.labels_
+
+        ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=labels.astype(np.float))
+
+        ax.w_xaxis.set_ticklabels([])
+        ax.w_yaxis.set_ticklabels([])
+        ax.w_zaxis.set_ticklabels([])
+        ax.set_xlabel('Petal width')
+        ax.set_ylabel('Sepal length')
+        ax.set_zlabel('Petal length')
+        fignum = fignum + 1
+
+    # Plot the ground truth
+    fig = plt.figure(fignum, figsize=(4, 3))
+    plt.clf()
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+    plt.cla()
+
+    for name, label in [('Setosa', 0),
+                        ('Versicolour', 1),
+                        ('Virginica', 2)]:
+        ax.text3D(X[y == label, 3].mean(),
+                  X[y == label, 0].mean() + 1.5,
+                  X[y == label, 2].mean(), name,
+                  horizontalalignment='center',
+                  bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+    # Reorder the labels to have colors matching the cluster results
+    y = np.choose(y, [1, 2, 0]).astype(np.float)
+    ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y)
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.set_xlabel('Petal width')
+    ax.set_ylabel('Sepal length')
+    ax.set_zlabel('Petal length')
+    plt.show()
 
 
 if __name__ == "__main__":
