bd22692de666dce5a38280efdc1b69bddd5f05f5
shoe116 <shupoppo.116@gmail.com>
2015-03-07 18:40:47 +0900

    bug fix and validate

--- kHLL/kestimate.py
+++ kHLL/kestimate.py
@@ -2,13 +2,16 @@ from HLL.hyperloglog import BaseHyperLogLog
 from abc import abstractmethod
 
 class BaseKEstimater(object):
-    def __init__(kmin, kmax, hashFunc):
+    def __init__(self, kmin, kmax, hashFunc):
         self.kmin = kmin
         self.kmax = kmax
         self.hashFunc = hashFunc
         self.k = 0
 
     def getK(self):
+        if self.k <= 0:
+            raise Exception("train failed")
+
         return int(self.k)
 
     @abstractmethod
