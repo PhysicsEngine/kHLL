69ea18fb85aa7383e9f06e59383813adc9ad6408
Lewuathe <lewuathe@me.com>
2015-03-08 10:08:00 +0900

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'kHLL',
   packages = ['kHLL'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Memory saving and fast k-deterministic k-means with HyperLogLog',
   author = 'Kai Sasaki, Shu Suzuki, Yutaka Yamada',
   author_email = 'lewuathe@me.com',
